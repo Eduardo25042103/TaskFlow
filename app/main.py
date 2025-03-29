8dd273ff31256df13bdc336cfbdef1309f978113
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-03-29 23:35:40 +0000

    include routes of tasks in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.routes import tasks
 
 app = FastAPI(title = "TaskFlow with Python and React", version = "1.0")
+
+
+app.include_router(tasks.router)
+
 
 @app.get("/")
 async def read_root():
