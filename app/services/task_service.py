9e6d260e379d9c99f25c7deacc833e911127a93e
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-03-30 22:51:04 +0000

    added function to delete in task_service.py

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -23,3 +23,20 @@ def update_task(db:Session, task_id: int, task_data: dict, current_user:models.U
     db.commit()
     db.refresh(task)
     return task
+
+def delete_task(db: Session, task_id: int, current_user: models.User):
+    """
+    Busca la tarea por ID que pertenezca al usuario autenticado.
+    Si la encuentra, la elimina y realiza commit en la base de datos.
+    Devuelve la tarea eliminada, o None si no se encontró.
+    """
+    task = db.query(models.Task).filter(
+        models.Task.id == task_id,
+        models.Task.user_id == current_user.id
+    ).first()
+    if not task:
+        return None
+    
+    db.delete(task)
+    db.commit()
+    return task
