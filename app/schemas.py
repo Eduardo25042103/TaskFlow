1b4265b2456f5812235b6a3c801b15a528e9b10c
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-03-30 03:45:36 +0000

    Modified schemas.py added user schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
 class TaskBase(BaseModel):
@@ -13,6 +13,21 @@ class Task(TaskBase):
     id: int
     completed: bool
     created_at: datetime
+
+    class Config:
+        orn_mode = True
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class User(BaseModel):
+    id: int
+    email: EmailStr
 
     class Config:
         orn_mode = True
