d775d67b7c9ed954f669346096d9aaa3171a20cb
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-03-30 05:28:36 +0000

    modified auth.py to obtain verified user

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import models, schemas
-from app.services.auth_service import hash_password, verify_password, create_access_token
+from app.services.auth_service import hash_password, verify_password, create_access_token, decode_access_token
 from datetime import timedelta
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
 @router.post("/register", response_model=schemas.User)
@@ -34,3 +36,18 @@ def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
     
     token = create_access_token(data={"sub": db_user.email}, expires_delta=timedelta(minutes=30))
     return {"access_token": token, "token_type": "bearer"}
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token")
+    
+    email = payload.get("sub")
+    if not email:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token payload")
+    
+    user = db.query(models.User).filter(models.User.email == email).first()
+    if user is None:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
+        
