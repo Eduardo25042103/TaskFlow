255cb274c6a17d7acd25cec9865ed3dd4ddbb808
Eduardo <124465964+Eduardo25042103@users.noreply.github.com>
2025-03-30 03:43:04 +0000

    Modified models.py add User table

--- app/models.py
+++ app/models.py
@@ -10,3 +10,11 @@ class Task(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     create_at = Column(DateTime, default=datetime.utcnow)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
